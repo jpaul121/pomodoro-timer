35062b70c900efe27f199f2811c3cac6f5808b82
jpaul121 <jpaulvalen+github@gmail.com>
2020-12-27 21:20:33 -0500

    Updates

---
+++ precache-manifest.fa81ea06ed3a6c666faf9d4f391dac1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c961b33ad5e06023c774e197f5e7f6dc",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "c09e2edeec8a1f95abe6",
+    "url": "/pomodoro-timer/static/css/main.d5b4a460.chunk.css"
+  },
+  {
+    "revision": "a8ffdd403a3dbbcc2121",
+    "url": "/pomodoro-timer/static/js/2.c72f3526.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/pomodoro-timer/static/js/2.c72f3526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c09e2edeec8a1f95abe6",
+    "url": "/pomodoro-timer/static/js/main.1789701d.chunk.js"
+  },
+  {
+    "revision": "5b934f73dc3f50a7b525",
+    "url": "/pomodoro-timer/static/js/runtime-main.cb3a558a.js"
+  }
+]);
